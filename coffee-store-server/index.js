9b96526f4cb4fcc7e65d5bb4d8e590f583d32eba
Rakib010 <hossainrokib55@gmail.com>
2024-11-28 22:43:41 +0600

    add comment

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -14,6 +14,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 /* console.log(uri); */
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
