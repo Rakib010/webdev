aaee2800eec950fafe734e517cb3f3c34b7cbbee
Rakib010 <hossainrokib55@gmail.com>
2025-01-01 11:38:40 +0600

    plant post & get

--- Milestone-12/plantNet/server/index.js
+++ Milestone-12/plantNet/server/index.js
@@ -48,6 +48,31 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    //
+    const db = client.db("plantNet");
+    const useCollection = db.collection("users");
+    const plantsCollection = db.collection("plants");
+
+    // save or update user in db
+    app.post("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+
+      // check if user exists in db
+      const isExist = await useCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist);
+      }
+
+      const result = await useCollection.insertOne({
+        ...user,
+        role: "customer",
+        timestamp: Date.now(),
+      });
+      res.send(result);
+    });
+
     // Generate jwt token
     app.post("/jwt", async (req, res) => {
       const email = req.body;
@@ -77,13 +102,18 @@ async function run() {
       }
     });
 
-    
+    // save a plant data  in db
+    app.post("/plants", verifyToken, async (req, res) => {
+      const plant = req.body;
+      const result = await plantsCollection.insertOne(plant);
+      res.send(result);
+    });
 
-
-
-
-
-
+    // get plant data  in db
+    app.get("/plants",async (req, res) => {
+      const result = await plantsCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
