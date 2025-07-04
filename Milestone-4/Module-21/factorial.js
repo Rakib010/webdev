d03cec0b8897bee1736a05b25f87bcc3c9433fb3
Rakib Hossain <hossainrokib55@gmail.com>
2025-07-04 18:15:42 +0600

    updated folder

--- Milestone-4/Module-21/factorial.js
+++
@@ -1,8 +0,0 @@
-function fact(n) {
-    
-  
-
-}
-const n = 5;
-const result = fact(n);
-console.log(result);
--- Milestone-4/Module-22/date.js
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-
-const date = new Date('2062-10-19')
-console.log(date.getDay());
-console.log(date.toLocaleDateString('en-GB'));
-
-// unix epoc 
